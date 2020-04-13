6a671af7a3a1465a68cba3175f9a0f46047e26fe
NBlaz <NBlaz@github.com>
2020-04-13 16:03:49 +0200

    refactoring to make more sense to the generated parts

---
+++ Src/os/os.c
@@ -0,0 +1,111 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * File Name          : os.c
+  * Description        : Code for freertos applications
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
+  *
+  ******************************************************************************
+  */
+#include "os.h"
+
+void os_create_tasks(void){
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* definition and creation of defaultTask */
+  const osThreadAttr_t defaultTask_attributes = {
+    .name = "defaultTask",
+    .stack_mem = &defaultTaskBuffer[0],
+    .stack_size = sizeof(defaultTaskBuffer),
+    .cb_mem = &defaultTaskControlBlock,
+    .cb_size = sizeof(defaultTaskControlBlock),
+    .priority = (osPriority_t) osPriorityNormal,
+  };
+  defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
+
+  /* definition and creation of myTask02 */
+  const osThreadAttr_t myTask02_attributes = {
+    .name = "myTask02",
+    .stack_mem = &myTask02Buffer[0],
+    .stack_size = sizeof(myTask02Buffer),
+    .cb_mem = &myTask02ControlBlock,
+    .cb_size = sizeof(myTask02ControlBlock),
+    .priority = (osPriority_t) osPriorityNormal1,
+  };
+  myTask02Handle = osThreadNew(StartTask02, NULL, &myTask02_attributes);
+
+  /* definition and creation of myTask02 */
+  const osThreadAttr_t myTask03_attributes = {
+    .name = "myTask03",
+    .stack_mem = &myTask03Buffer[0],
+    .stack_size = sizeof(myTask03Buffer),
+    .cb_mem = &myTask03ControlBlock,
+    .cb_size = sizeof(myTask03ControlBlock),
+    .priority = (osPriority_t) osPriorityNormal,
+  };
+  myTask03Handle = osThreadNew(StartTask03, NULL, &myTask03_attributes);
+
+}
+
+void StartDefaultTask(void *argument)
+{
+
+  /* USER CODE BEGIN 5 */
+  /* Infinite loop */
+  for(;;)
+  {
+    //refresh_screen();  
+    readTouch();
+    osDelay(1);
+  }
+  /* USER CODE END 5 */ 
+}
+
+void StartTask02(void *argument)
+{
+  /* USER CODE BEGIN StartTask02 */
+  /* Infinite loop */
+  
+  for(;;)
+  {    
+    refresh_screen();
+    osDelay(1);
+  }
+  /* USER CODE END StartTask02 */
+}
+
+void StartTask03(void *argument)
+{
+  /* USER CODE BEGIN StartTask02 */
+  /* Infinite loop */
+  for(;;)
+  {
+    //TM_ILI9341_Fill(ILI9341_COLOR_GREEN2);
+    osDelay(1);
+  }
+  /* USER CODE END StartTask02 */
+}
---
+++ Src/os/os.h
@@ -0,0 +1,30 @@
+#ifndef _OS_H
+#define _OS_H
+
+#include "cmsis_os.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "touch.h"
+#include "ux.h"
+
+typedef StaticTask_t osStaticThreadDef_t;
+
+osThreadId_t defaultTaskHandle;
+uint32_t defaultTaskBuffer[ 128 ];
+osStaticThreadDef_t defaultTaskControlBlock;
+
+osThreadId_t myTask02Handle;
+uint32_t myTask02Buffer[ 128 ];
+osStaticThreadDef_t myTask02ControlBlock;
+
+osThreadId_t myTask03Handle;
+uint32_t myTask03Buffer[ 128 ];
+osStaticThreadDef_t myTask03ControlBlock;
+
+void os_create_tasks(void);
+
+void StartDefaultTask(void *argument);
+void StartTask02(void *argument);
+void StartTask03(void *argument);
+
+#endif
